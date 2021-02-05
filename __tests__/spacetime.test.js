bb1a7f96aedc62f965b8e435daab8f6d0f58bb6e
Dani Renner <danijrenner@gmail.com>
2021-02-05 11:21:30 -0800

    add test for life expectancy property: good fail

--- __tests__/spacetime.test.js
+++ __tests__/spacetime.test.js
@@ -2,10 +2,11 @@ import Age from './../src/js/spacetime.js';
 describe('Age', () => {
   let ageObject;
   beforeEach(() => {
-    ageObject = new Age(55);
+    ageObject = new Age(55, 73);
   });
   test('should correctly create an Age object', () => {
   expect(ageObject.age).toEqual(55);
+  expect(ageObject.lifeExpectancy).toEqual(73);
   });
   test('should correctly calculate a users age in Mercury years', () => {
   expect(ageObject.mercuryAge()).toEqual(229);

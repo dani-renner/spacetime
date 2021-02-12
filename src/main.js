6e7fef4a2a62b91cfb38c6b1a08e1597c13f7cc2
Dani Renner <danijrenner@gmail.com>
2021-02-11 18:52:01 -0800

    update UI to use ageCalculator()

--- src/main.js
+++ src/main.js
@@ -7,17 +7,18 @@ import Age from './js/spacetime.js';
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
     event.preventDefault();
+    
     const userAge = parseInt($('#ageInput').val());
     const userLifeExpectancy = parseInt($('#lifeExpectancyInput').val());
     const userData = new Age(userAge, userLifeExpectancy);
     $(".output").show();
-    $("#userMercury").text(userData.mercuryAge());
-    $("#mercuryLeft").text(userData.lifeLeft((userData.mercuryAge()), (userData.mercuryLifeExpentancy())));
-    $("#userVenus").text(userData.venusAge());
-    $("#venusLeft").text(userData.lifeLeft((userData.venusAge()), (userData.venusLifeExpentancy())));
-    $("#userMars").text(userData.marsAge());
-    $("#marsLeft").text(userData.lifeLeft((userData.marsAge()), (userData.marsLifeExpentancy())));
-    $("#userJupiter").text(userData.jupiterAge());
-    $("#jupiterLeft").text(userData.lifeLeft((userData.jupiterAge()), (userData.jupiterLifeExpentancy())));
+    $("#userMercury").text(userData.ageCalculator("mercury"));
+    $("#mercuryLeft").text(userData.lifeLeft((userData.ageCalculator("mercury")), (userData.mercuryLifeExpentancy())));
+    $("#userVenus").text(userData.ageCalculator("venus"));
+    $("#venusLeft").text(userData.lifeLeft((userData.ageCalculator("venus")), (userData.venusLifeExpentancy())));
+    $("#userMars").text(userData.ageCalculator("mars"));
+    $("#marsLeft").text(userData.lifeLeft((userData.ageCalculator("mars")), (userData.marsLifeExpentancy())));
+    $("#userJupiter").text(userData.ageCalculator("jupiter"));
+    $("#jupiterLeft").text(userData.lifeLeft((userData.ageCalculator("jupiter")), (userData.jupiterLifeExpentancy())));
   });
 });

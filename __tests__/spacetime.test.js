fabde1503111aa7012728e07f5736932b471da31
Dani Renner <danijrenner@gmail.com>
2021-02-11 18:24:10 -0800

    fix indentation on 'expect' statements

--- __tests__/spacetime.test.js
+++ __tests__/spacetime.test.js
@@ -5,36 +5,36 @@ describe('Age', () => {
     ageObject = new Age(55, 73);
   });
   test('should correctly create an Age object', () => {
-  expect(ageObject.age).toEqual(55);
-  expect(ageObject.lifeExpectancy).toEqual(73);
+    expect(ageObject.age).toEqual(55);
+    expect(ageObject.lifeExpectancy).toEqual(73);
   });
   test('should correctly calculate a users age in Mercury years', () => {
-  expect(ageObject.mercuryAge()).toEqual(229);
+    expect(ageObject.mercuryAge()).toEqual(229);
   });
   test('should correctly calculate a users age in Venus years', () => {
-  expect(ageObject.venusAge()).toEqual(89);
+    expect(ageObject.venusAge()).toEqual(89);
   });
   test('should correctly calculate a users age in Mars years', () => {
-  expect(ageObject.marsAge()).toEqual(29);
+    expect(ageObject.marsAge()).toEqual(29);
   });
   test('should correctly calculate a users age in Jupiter years', () => {
-  expect(ageObject.jupiterAge()).toEqual(5);
+    expect(ageObject.jupiterAge()).toEqual(5);
   });
   test('should correctly calculate a users age in Mercury years', () => {
-  expect(ageObject.mercuryLifeExpentancy()).toEqual(304);
+    expect(ageObject.mercuryLifeExpentancy()).toEqual(304);
   });
   test('should correctly calculate a users age in Venus years', () => {
-  expect(ageObject.venusLifeExpentancy()).toEqual(118);
+    expect(ageObject.venusLifeExpentancy()).toEqual(118);
   });
   test('should correctly calculate a users age in Mars years', () => {
-  expect(ageObject.marsLifeExpentancy()).toEqual(39);
+    expect(ageObject.marsLifeExpentancy()).toEqual(39);
   });
   test('should correctly calculate a users age in Jupiter years', () => {
-  expect(ageObject.jupiterLifeExpentancy()).toEqual(6);
+    expect(ageObject.jupiterLifeExpentancy()).toEqual(6);
   });
   test('should correctly calculate years left in users life', () => {
-  expect(ageObject.lifeLeft(55,73)).toEqual("18 years until death");
-  expect(ageObject.lifeLeft(55,55)).toEqual("Time's up.")
-  expect(ageObject.lifeLeft(78,73)).toEqual("You've outlived your life expectancy by 5 years!");
+    expect(ageObject.lifeLeft(55,73)).toEqual("18 years until death");
+    expect(ageObject.lifeLeft(55,55)).toEqual("Time's up.")
+    expect(ageObject.lifeLeft(78,73)).toEqual("You've outlived your life expectancy by 5 years!");
   });
 });
